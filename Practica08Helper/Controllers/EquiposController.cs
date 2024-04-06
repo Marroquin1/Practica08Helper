60364331904e398098118f9e6564bdb5e0768cc1
Marroquin1 <luis.marroquin@catolica.edu.sv>
2024-04-05 22:30:42 -0600

    Agregar archivos de proyecto.

---
+++ Practica08Helper/Controllers/EquiposController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica08Helper.Controllers
+{
+    public class EquiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
